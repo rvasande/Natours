871d12cfbba06702da8f4911263d0323b2eaebfc
rushikesh <rushikeshvasande333@gmail.com>
2024-07-11 23:37:30 +0530

    build template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,22 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) get data from Tour collection
+  const tours = await Tour.find();
+
+//   2) build template 
+
+// 3) render that template using data from step 1
+
   res.status(200).render("overview", {
     title: "All tours",
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   res.status(200).render("tour", {
     title: "The forest hiker",
   });
-};
+});
